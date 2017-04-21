b3311396f9aec1010dfe4ee19f1fad6996ba0257
Mesta17 <mark.ghareeb@gmail.com>
2017-04-20 23:44:01 -0400

    added debugMode, made verbose actually make sense

--- main.go
+++ main.go
@@ -10,16 +10,22 @@ import (
 	"github.com/Mesta17/ServiceDiscovery/xlog"
 )
 
+const debugMode = true
+
 type handler func(http.ResponseWriter, *http.Request)
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	xlog.Infof("\n\tMethod:%v \n\tURL:%v", r.Method, r.URL.Path)
-	recorder := httptest.NewRecorder()
-	h(recorder, r)
-	response := recorder.Body
-	body, _ := ioutil.ReadAll(response)
-	xlog.Verbosef("%v", string(body))
-	fmt.Fprintf(w, string(body))
+	if debugMode {
+		recorder := httptest.NewRecorder()
+		h(recorder, r)
+		response := recorder.Body
+		body, _ := ioutil.ReadAll(response)
+		xlog.Verbosef("\n\tResponse:%v", string(body))
+		fmt.Fprintf(w, string(body)+"\n")
+	} else {
+		h(w, r)
+	}
 }
 
 func loveHandler(w http.ResponseWriter, r *http.Request) {
--- xlog/log.go
+++ xlog/log.go
@@ -15,13 +15,15 @@ import (
 // Infof provides info level log statement
 func Infof(format string, a ...interface{}) (int, error) {
 	logTime := time.Now().Format("01/02 15:04:05")
-	formatted := fmt.Sprintf("Info %v: ", logTime)
+	formatted := fmt.Sprintf("Info    %v: ", logTime)
 	return fmt.Printf(formatted+format+"\n", a...)
 }
 
 // Verbosef provides verbose level log statement
 func Verbosef(format string, a ...interface{}) (int, error) {
-	return fmt.Printf(format, a...)
+	logTime := time.Now().Format("01/02 15:04:05")
+	formatted := fmt.Sprintf("Verbose %v: ", logTime)
+	return fmt.Printf(formatted+format+"\n", a...)
 }
 
 // Warningf provides warning level log statment
